ab11eb3729c817bdf56afd26a91f9411d8dd2c80
Helix Graziani <helix.graziani@hotmail.com>
2025-04-07 20:36:26 -0400

    add ContentProvider.BaseUrl

--- include/contentprovider.h
+++ include/contentprovider.h
@@ -5,9 +5,12 @@
 
 typedef struct ContentProvider {
     Instance instance;
+
+    const char *BaseUrl;
 } ContentProvider;
 
 ContentProvider *ContentProvider_new(const char *className, Instance *parent);
+void serialize_ContentProvider(ContentProvider *contentprovider, SerializeInstance *inst);
 
 #endif
 
--- src/contentprovider.c
+++ src/contentprovider.c
@@ -2,6 +2,23 @@
 
 ContentProvider *ContentProvider_new(const char *className, Instance *parent)
 {
-    INSTANCE_STUB_CONSTRUCTOR(ContentProvider, Instance);
+    ContentProvider *newInst = Instance_new(className, parent);
+
+    newInst->instance.DataCost = sizeof(*newInst);
+    newInst = realloc(newInst, sizeof(*newInst));
+
+    newInst->BaseUrl = "http://www.roblox.com";
+
+    if (parent)
+        Instance_SetParent(newInst, parent);
+
+    return newInst;
+}
+
+void serialize_ContentProvider(ContentProvider *contentprovider, SerializeInstance *inst)
+{
+    serialize_Instance(contentprovider, inst);
+
+    serialize_atomic(string, contentprovider, BaseUrl);
 }
 
